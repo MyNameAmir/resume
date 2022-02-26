de809930a21610d0398a7f24530c96867a6449b2
Amirali Moin <amir@Amiralis-MacBook-Pro.local>
2022-02-26 12:04:43 -0800

    smooth scroll to each of the components of the nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // App.js
 import React from 'react';
 import axios from 'axios';
+import { useRef } from 'react';
 
 class App extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -10,8 +12,16 @@ class App extends React.Component {
             commentSuccessMessage: ""
         }
 
+        this.homeRef = React.createRef();
+        this.aboutRef = React.createRef();
+        this.workRef = React.createRef();
+        this.futureRef = React.createRef();
+        this.contactRef = React.createRef();
+
+
         this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
         this.handleCommentChange = this.handleCommentChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleCommentChange(event) {
@@ -37,34 +47,68 @@ class App extends React.Component {
 
     }
 
+
+    handleClick(event){
+       
+       
+        switch (event.target.innerHTML.toLowerCase()){
+            case 'about':
+                document.getElementById('about').scrollIntoView(true);
+                break;
+            case 'home':
+                window.scrollTo(0, this.homeRef.current);
+                break;
+            case 'work':
+                document.getElementById('work').scrollIntoView(true);
+                break;  
+            case 'future':
+                document.getElementById('future').scrollIntoView(true);
+                break; 
+            case 'contact':
+                document.getElementById('contact').scrollIntoView(true);
+                break; 
+        }
+  
+        
+
+    }
+
     render() {
+
+
+
         return <>
             <div className="wrapper">
                 <div id="navigation">
                     <ul className="nav">
-                        <li><a href="#top">Home</a></li>
+                        {/* <li><a href="#top">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#work">Work</a></li>
                         <li><a href="#future">Future</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#contact">Contact</a></li> */}
+                        <li><div onClick={this.handleClick}>home</div></li>
+                        <li><div onClick={this.handleClick}>about</div></li>
+                        <li><div onClick={this.handleClick}>work</div></li>
+                        <li><div onClick={this.handleClick}>future</div></li>
+                        <li><div onClick={this.handleClick}>contact</div></li>
                     </ul>
                 </div>
                 <div id="everythingElse">
-                    <div className="intro" id="top">
+                    <div className="intro" id="home" ref={this.homeRef}>
                         <div id="restOfIntro">
                             <img className="circularImage" width="120px" height="120px" src="amir.png" alt="main image" />
                             <h1>Amir Moin</h1>
                             <span className='pulse-button'><a id="pulseA" href="#about">Start Here</a></span>
                         </div>
                     </div>
-                    <h2 id="about"><strong><i>A</i></strong> little bit about myself</h2>
+                    <h2 id="about" ref={this.aboutRef}><strong><i>A</i></strong> little bit about myself</h2>
                     <p className="para"><strong>Greetings</strong> weary traveller, welcome to my page! 😊 My name is Amirali Moin, and I consider myself to be an avid learner!
             I can, with full confidence, say that I am a hard working person who pays attention to detail. I love to code and make websites and it was something
             that started as a hobby for me and I want to pursue it professionally. I am most knowledgable in front end web development using <strong>HTML</strong>, <strong>CSS</strong>, <strong>Javascript</strong> and <strong>React</strong>,
             but also good at backend languages such as <strong>Node.js</strong> and good with database web dev usage such as MongoDB. I am also very good at Java programming. Just to add one more thing here, I am an avid follower of soccer and I have been playing the
             game since I was 6 years old! and I do not plan on stopping until much much later into my life. </p>
                     <img className="linearImage" width="100%" src="image1.png" alt="image" />
-                    <h2 id="work"><strong><i>A</i></strong> little bit about my previous work</h2>
+                    <h2 id="work" ref={this.workRef}><strong><i>A</i></strong> little bit about my previous work</h2>
                     <p className="para"><strong>As</strong> I previously mentioned, I have worked with HTML, CSS, JS, React framework and Node.js. and with those tools I have created
             2 projects and this exact page you are on. The first project I would like to show you is the <a className="inParagraphA" href="http://travellinglog.herokuapp.com/" target="_blank">travellinglog</a> where
             you can access this single page app and talk about the countries you have visited and upload images of the place you have been to so you can showcase to the world. It is
@@ -81,13 +125,13 @@ class App extends React.Component {
             can manage and track any tasks that they encounter daily and each user can manage their own persons on this app and assign tasks to them. And you can remove tasks on this app
             once a task is achieved.</p>
                     <img className="linearImage" width="100%" src="image6.png" alt="image" />
-                    <h2 id="future"><strong><i>A</i></strong> little bit about my future prospects</h2>
+                    <h2 id="future" ref={this.futureRef}><strong><i>A</i></strong> little bit about my future prospects</h2>
                     <p className="para">In the near future I hope to be able to find work as a web developer, if it is front-end, back-end or full-stack. Having worked in the educational
             field of development and computer science, I am eager to make the jump to complete work in the industry. My goal as always is to learn more and more and by getting employed and being in an all web development setting would be ideal to me as I am passionate
             about web development and programming. I would also love to connect with others passionate about everything web dev so if you are also
             a web dev head like me, connect with me through my links in the <a className="inParagraphA" href="#contact">contact</a> section. Would love to meet more people and network!</p>
                     <img className="linearImage" width="100%" src="image3.png" alt="image" />
-                    <h2 id="contact"><strong><i>Ways</i></strong> to contact me</h2>
+                    <h2 id="contact" ref={this.contactRef}><strong><i>Ways</i></strong> to contact me</h2>
                     <p className="para" id="footerP"> you can contact me on these amazing platforms or leave a comment down below: </p>
                     <a href="https://www.instagram.com/ameer.ca/" target="_blank"><img className="circularImage" width="64px" src="icon1.png" alt="image" /></a>
                     <a href="https://www.linkedin.com/in/amirali-moin-74b285138/" target="_blank"><img className="circularImage" width="64px" src="icon2.png" alt="image" /></a>
